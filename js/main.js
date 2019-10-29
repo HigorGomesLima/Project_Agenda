62cbd5b17dc8b7d83f5696c2f17c659c0c616586
HigorGomesLima <37384951+HigorGomesLima@users.noreply.github.com>
2019-10-29 18:59:11 -0300

    Index
    Ajustes dos get para envio no db

--- js/main.js
+++ js/main.js
@@ -1,18 +1,45 @@
-$(document).ready(function(){
-    $('.ipt-telefone').mask('(00) 0000-0000');
-});
-
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
+(function init(){
+    $(".ipt-data input").val(moment().format('DD/MM/YYYY'));
+})();
 
 $(".btn-cadastrar").click(function (){
     let nome = $(".ipt-nome input").val();
     let convenio = $(".ipt-convenio select").val();
     let serie = $(".ipt-serie select").val();
     let contatos = [];
-    /*$(".multiple-form-group").forEach((contact) => {
-        console.log(contact);
-    })*/
-    console.log($(".multiple-form-group")[1]);
+    $(".detalhe-contato").each(function (){
+        let aux = {
+            telefone: $(this).find(".ipt-telefone").val(),
+            contato: $(this).find(".ipt-contato").val()
+        }
+        contatos.push(aux);
+    });
+    let data = $(".ipt-data input").val();
+    let escola = $(".ipt-escola input").val();
+    let motivo = $(".ipt-motivo textarea").val();
+    let boletim = $(".ipt-boletim select").val();
+    let disciplinas = [];
+    $(".ms-selection .ms-list .ms-selected span").each(function (){
+        disciplinas.push(this.innerText);
+    });
+    let talento = $(".ipt-talento textarea").val();
+    let observacao = $(".ipt-observacao textarea").val();
+    let situacao = $(".ipt-situacao select").val();
+    
+    let candidato = {
+        nome: nome,
+        convenio: convenio,
+        serie: serie,
+        contatos: contatos,
+        data: data,
+        escola: escola,
+        motivo: motivo,
+        boletim: boletim,
+        disciplinas: disciplinas,
+        talento: talento,
+        observacao: observacao,
+        situacao: situacao
+    }
+    
+    console.log(candidato);
 })
